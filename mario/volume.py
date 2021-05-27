1e454a34824fee38b204e694538739e4130a83a0
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-05-27 13:35:34 -0400

    add vol

---
+++ mario/volume.py
@@ -0,0 +1,45 @@
+import kfp.dsl as dsl
+from typing import *
+from enum import Enum
+
+
+class VolumeMode(Enum):
+    ReadOnlyMany = "r"
+    ReadWriteMany = "rw"
+    ReadWriteOnce = "rw1"
+
+
+_enum_to_dsl_volume_mode = {
+    VolumeMode.ReadOnlyMany: dsl.VOLUME_MODE_ROM,
+    VolumeMode.ReadWriteMany: dsl.VOLUME_MODE_RWM,
+    VolumeMode.ReadWriteOnce: dsl.VOLUME_MODE_RWO,
+}
+
+
+def claim(
+    name: str, size: str, mode: Union[str, VolumeMode] = "rw", **kwargs
+) -> dsl.PipelineVolume:
+    if isinstance(mode, str):
+        mode = VolumeMode(mode)
+    assert isinstance(
+        mode, VolumeMode
+    ), "Argument `mode` must be an instance of `VolumeMode`."
+
+    human_name = display_name = name
+    resource_name = f"pvc-{name}-{size}"
+
+    vop = dsl.VolumeOp(
+        name=human_name,
+        resource_name=resource_name,
+        size=size,
+        modes=_enum_to_dsl_volume_mode[mode],
+        **kwargs,
+    )
+
+    vop.set_display_name(display_name)
+
+    return vop.volume
+
+
+def load(name: str, **kwargs) -> dsl.PipelineVolume:
+    return dsl.PipelineVolume(pvc=name, **kwargs)
