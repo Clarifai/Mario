1f3b8ef7a9b46e1a0b7f2037f690b93d23d36311
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-05-28 15:06:26 -0400

    refactor volume op

--- mario/node/volume.py
+++ mario/node/volume.py
@@ -1,10 +1,11 @@
 from ..volume import *
 from .base import Node
 from typing import *
+import kfp.dsl as dsl
 
 
-class VolumeNode(Node):
-    """VolumeNode: Wrapper of VolumeOp"""
+class ClaimVolume(Node):
+    """ClaimVolume: Wrapper of VolumeOp"""
 
     def __init__(
         self, name: str, size: str, mode: Union[str, VolumeMode] = "rw", **kwargs
@@ -14,8 +15,24 @@ class VolumeNode(Node):
         self.size = size
         self.mode = mode
         self._kwargs = kwargs
-        self.volume = load("future_vol_placeholder")
+        self.volume = None
 
-    def flow(self) -> None:
+    def flow(self) -> dsl.PipelineVolume:
 
         self.volume = claim(self.name, self.size, self.mode, **self._kwargs)
+
+        return self.volume
+
+
+class LoadVolume(Node):
+    """LoadVolume: load existing volume"""
+
+    def __init__(self, name: str, **kwargs) -> None:
+        super().__init__()
+        self.name = name
+        self._kwargs = kwargs
+        self.volume = load(name, **kwargs)
+
+    def flow(self) -> dsl.PipelineVolume:
+
+        return self.volume
