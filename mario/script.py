1d7c0e5a274a571933072c7cd7ebf1abf00fedfa
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-05-29 15:04:34 -0400

    autofilename

--- mario/script.py
+++ mario/script.py
@@ -5,11 +5,14 @@ from typing import *
 __all__ = ["save"]
 
 
-def save(function: Callable, filename: str, **kwargs) -> None:
+def save(function: Callable, filename: Optional[str] = None, **kwargs) -> None:
     """Turn annotated Callable class to pipeline function."""
     name = function.__name__
     description = function.__doc__
 
     tmp = kfp.dsl.pipeline(name=name, description=description)(function)
+
+    if not filename:
+        filename = name.replace("_", "-") + ".yaml"
 
     kfp.compiler.Compiler().compile(tmp, filename, **kwargs)
