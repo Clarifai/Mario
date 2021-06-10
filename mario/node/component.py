122309d732ac16e26b65c83f4773e0dcd37b52f0
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-06-10 19:40:57 -0400

    add isort

--- mario/node/component.py
+++ mario/node/component.py
@@ -1,3 +1,5 @@
+import yaml
+
 from .compute import Compute
 
 __all__ = ["Component"]
@@ -6,4 +8,10 @@ __all__ = ["Component"]
 class Component(Compute):
     @classmethod
     def from_component_yaml(cls, path_to_file):
-        pass
+        comp = yaml.safe_load(open(path_to_file, "r").read())
+        name = comp["name"]
+        imag = comp["image"]
+        args = [d["name"] for d in comp["inputs"]]
+        cmnd = comp["implementation"]["command"]
+
+        return cls(name=name, image=imag, command=cmnd, arg_names=args)
--- mario/node/compute.py
+++ mario/node/compute.py
@@ -1,6 +1,9 @@
-from .base import Node
 from typing import *
+
 import kfp.dsl as dsl
+import yaml
+
+from .base import Node
 
 __all__ = ["Compute"]
 
@@ -86,3 +89,13 @@ class Compute(Node):
 
     def __len__(self):
         return len(self.volumes)
+
+    @classmethod
+    def from_component_yaml(cls, path_to_file):
+        comp = yaml.safe_load(open(path_to_file, "r").read())
+        name = comp["name"]
+        imag = comp["image"]
+        args = [d["name"] for d in comp["inputs"]]
+        cmnd = comp["implementation"]["command"]
+
+        return cls(name=name, image=imag, command=cmnd, arg_names=args)
