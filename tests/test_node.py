7438cb23a852fa62e2ba7dcc1ac45d16a4dacef6
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-06-10 18:16:16 -0400

    add test

---
+++ tests/test_node.py
@@ -0,0 +1,19 @@
+import mario
+
+
+def test_volume():
+    v0 = mario.node.VolumeLoad("existing-vol")
+    v1 = mario.node.VolumeClaim("new-vol", "1Gi")
+
+    print(v0)
+    print(v1)
+
+
+def test_compute_node():
+    f0 = mario.node.Compute(
+        "f0",
+        "your-registry:tag",
+        command=["python3", "/entrypoint/main.py"],
+        arg_names=["arg1", "arg2"],
+    )
+    print(f0)
